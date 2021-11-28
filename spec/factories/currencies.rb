6d505c437edc62af342fe37fdbd8896d375ddbfe
Atix <atixan12gmail.com>
2021-11-28 16:41:19 +0400

    fix methods

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :currency do
-    sequence(:phone) { |i| 70000000000 + i }
-    num_code { "MyString" }
-    char_code { "MyString" }
-    nominal
-    name { "2021-11-06 17:32:38" }
-    value { "my_comment" }
+    # sequence(:phone) { |i| 70000000000 + i }
+    num_code { "777" }
+    char_code { "MYC" }
+    nominal {100}
+    name { "My Currency" }
+    value { 12.12 }
   end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Currency, type: :model do
   describe 'validations' do
     subject { build(:currency) }
-    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:num_code) }
+    it { should validate_presence_of(:char_code) }
+    it { should validate_presence_of(:nominal) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:value) }
   end
 end
